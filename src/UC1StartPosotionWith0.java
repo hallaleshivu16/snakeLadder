4b98ec12e4a38b9feb35c1c09fae53dc8c160627
shivkumar <hallaleshivu16@gmail.com>
2022-01-02 16:49:46 +0530

    ADDED: Game start with Posotion 0

---
+++ src/UC1StartPosotionWith0.java
@@ -0,0 +1,7 @@
+public class UC1StartPosotionWith0 {
+    public static void main(String[] args) {
+        System.out.println("Welcome to snake ladder game");
+        final int NUMBER_OF_PLAYER = 1;
+        final int START_POSITION = 0;
+    }
+}
