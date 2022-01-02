9237b710e9ad9d1c5d34254d04a6c9b0b712786e
shivkumar <hallaleshivu16@gmail.com>
2022-01-02 17:20:37 +0530

    ADDED: Roll Dies Number

--- src/com/bridgelabz/UC2RandomDiesNumber.java
+++ src/com/bridgelabz/UC2RandomDiesNumber.java
@@ -1,9 +1,15 @@
+package com.bridgelabz;
+
 import java.util.Random;
 
-public class UC1StartPosotionWith0 {
+public class UC2RandomDiesNumber {
     public static void main(String[] args) {
+
         System.out.println("Welcome to snake ladder game");
         final int NUMBER_OF_PLAYER = 1;
         final int START_POSITION = 0;
+        Random random = new Random();
+        int rollDie = random.nextInt(1,7);
+        System.out.println("Dies Number: " + rollDie);
     }
 }
